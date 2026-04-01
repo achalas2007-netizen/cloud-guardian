2080650f366763d34225e91e68843ead39e90725
achalas2007-netizen <achalas2007@gmail.com>
2026-04-01 11:05:09 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("🚀 Cloud Guardian - HITL System")
+
+st.write("If you see this, your app is working!")
+
+# Simple test button
+if st.button("Click Me"):
+    st.success("Button works!")
